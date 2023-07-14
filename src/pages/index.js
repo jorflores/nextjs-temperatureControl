6c24d1609a3cf1528f54addb126531a55e89c556
Jorge Flores <flores.jorgeluis@gmail.com>
2023-07-14 15:11:06 -0600

    se agregaron instrucciones del ejercicio

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,25 @@
 import React, { useState } from "react";
 
 export default function Home() {
+  /* 
+
+1) Reemplaza estas variables por variables de estado utilizando useState. 
+   Puedes utilizar los valores iniciales de "cold" y 10 
+   
+*/
+
   let temperatureColor = "cold";
   let temperatureValue = 10;
+
+  /* 
+  
+  2) Registra eventos "onClick" y las funciones correspondientes para los dos botones. 
+     Uno incrementará el valor y el otro lo decrementará. 
+     Dentro de las funciones para incrementar y decrementar, agrega una condición para que al momento que la temperatura se encuentre
+     en 20 grados, la variable 'temperatureColor' cambie a 'hot'. De igual manera, si la temperatura es < 20 grados, la variable 
+    cambia a 'cold'. 
+  
+  */
 
   return (
     <div className="app_container">
